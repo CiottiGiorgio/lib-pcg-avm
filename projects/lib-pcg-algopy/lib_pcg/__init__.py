9e03fffc11ffb364dce2c88717c1deb8075f6c46
CiottiGiorgio <giorgio.ciotti@algorand.foundation>
2024-04-06 17:53:09 +0200

    Export library code.

--- projects/lib-pcg-algopy/lib_pcg/__init__.py
+++ projects/lib-pcg-algopy/lib_pcg/__init__.py
@@ -1,4 +1,12 @@
 from .xsh_rr_64_32 import pcg32_init, pcg32_random
 from .xsh_rr_double_64_32 import pcg64_init, pcg64_random
+from .xsh_rr_quadruple_64_32 import pcg128_init, pcg128_random
 
-__all__ = ["pcg32_init", "pcg32_random", "pcg64_init", "pcg64_random"]
+__all__ = [
+    "pcg32_init",
+    "pcg32_random",
+    "pcg64_init",
+    "pcg64_random",
+    "pcg128_init",
+    "pcg128_random",
+]
