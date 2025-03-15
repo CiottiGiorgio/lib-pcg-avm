6bd8bd62dfb86db951574fb08de6b6cbf56cabb2
CiottiGiorgio <giorgio.ciotti@algorand.foundation>
2025-03-15 13:20:38 +0100

    refreshed algopy template.

--- projects/lib-pcg-algopy/lib_pcg/__init__.py
+++ projects/lib-pcg-algopy/lib_pcg/__init__.py
@@ -10,12 +10,12 @@ from .pcg64 import pcg64_init, pcg64_random
 from .pcg128 import pcg128_init, pcg128_random
 
 __all__ = [
-    "pcg32_init",
-    "pcg16_init",
     "pcg8_init",
-    "pcg32_random",
-    "pcg16_random",
     "pcg8_random",
+    "pcg16_init",
+    "pcg16_random",
+    "pcg32_init",
+    "pcg32_random",
     "pcg64_init",
     "pcg64_random",
     "pcg128_init",
