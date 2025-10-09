e53b439f711b9a026a1367d455585752d3b5a7e4
CiottiGiorgio <gciotti.dev@gmail.com>
2025-10-09 12:15:17 +0200

    update author info; packaging info for TS; deployment pipeline for TS

--- projects/lib-pcg-algots/eslint.config.mjs
+++ projects/lib-pcg-algots/eslint.config.mjs
@@ -2,17 +2,13 @@ import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import globals from 'globals'
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-      },
-    },
-    rules: {
-      '@typescript-eslint/explicit-member-accessibility': 'warn',
+export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended, {
+  languageOptions: {
+    globals: {
+      ...globals.node,
     },
   },
-);
+  rules: {
+    '@typescript-eslint/explicit-member-accessibility': 'warn',
+  },
+})
---
+++ projects/lib-pcg-algots/lib_pcg/index.ts
@@ -0,0 +1,3 @@
+export { pcg8Init, pcg8Random, pcg16Init, pcg16Random, pcg32Init, pcg32Random } from './pcg32.algo';
+export { pcg64Init, pcg64Random } from './pcg64.algo';
+export { pcg128Init, pcg128Random } from './pcg128.algo';
